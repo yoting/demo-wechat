45933e25d8aa322946a7536f904d6750562d95f5
dyy_gusi <dyy_gusi@sian.com>
2016-12-20 22:49:38 +0800

    get openid by page author

--- src/main/java/com/gusi/demo/controller/CommonController.java
+++ src/main/java/com/gusi/demo/controller/CommonController.java
@@ -1,5 +1,7 @@
 package com.gusi.demo.controller;
 
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.gusi.demo.config.WechatConfig;
+import com.gusi.demo.menu.Menu;
 import com.gusi.demo.thrid.wechat.WechatAPI;
 
 @RestController
@@ -28,16 +31,28 @@ public class CommonController {
 	@RequestMapping("/cfg/{id}")
 	public String config(@PathVariable("id") int id) {
 		if (id == 1) {
-			WechatAPI.createMenu(null, null);
-			return "success";
+			WechatAPI.createMenu(WechatAPI.getBaseInfo(null).get("token"),
+					Menu.getDemoMenu());
+			return "create menu success!";
 		} else {
 			return "no commnd find!";
 		}
 	}
 
 	@RequestMapping("/business")
-	public String business(@RequestParam(name = "code", required = false, defaultValue = "") String code,
+	public String business(
+			@RequestParam(name = "code", required = false, defaultValue = "") String code,
 			@RequestParam(name = "state", required = false, defaultValue = "") String state) {
-		return "sucess->code:" + code + ";state" + state;
+		System.out.println(code);
+		return "sucess->code:" + code + ";state:" + state;
+	}
+
+	@RequestMapping("/userinfo/{code}")
+	public String getUserInfo(@PathVariable(name = "code") String code) {
+		Map<String, String> data = WechatAPI.getBaseInfo(code);
+		String token = data.get("token");
+		String openid = data.get("openid");
+		return "get user info success!\r\n token:" + token + ";openid:"
+				+ openid;
 	}
 }
--- src/main/java/com/gusi/demo/thrid/wechat/WechatAPI.java
+++ src/main/java/com/gusi/demo/thrid/wechat/WechatAPI.java
@@ -1,8 +1,10 @@
 package com.gusi.demo.thrid.wechat;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import com.alibaba.fastjson.JSONObject;
 import com.gusi.demo.config.WechatConfig;
-import com.gusi.demo.menu.Menu;
 import com.gusi.demo.utils.HttpClientUtil;
 
 public class WechatAPI {
@@ -13,35 +15,50 @@ public class WechatAPI {
 	public static String ACCESS_TOKEN_CODE_URL = "https://api.weixin.qq.com/sns/oauth2/access_token?appid=APPID&secret=APPSECRET&code=CODE&grant_type=authorization_code";
 	public static String MENU_URL = "https://api.weixin.qq.com/cgi-bin/menu/create?access_token=ACCESS_TOKEN";
 
-	public static String getAccessToken(String code) {
-		if (code == null || code.isEmpty()) {
-			String urlStr = ACCESS_TOKEN_URL.replace(APPID, WechatConfig.getInstance().getAppid()).replace(APPSECRET,
+	/**
+	 * 获取微信基本信息 <br>
+	 * access_token:token<br>
+	 * openid:openid
+	 * 
+	 * @param code
+	 * @return Map
+	 */
+	public static Map<String, String> getBaseInfo(String code) {
+		Map<String, String> result = new HashMap<String, String>();
+		if (code == null || code.isEmpty()) {// 获取通用token
+			String urlStr = ACCESS_TOKEN_URL.replace(APPID,
+					WechatConfig.getInstance().getAppid()).replace(APPSECRET,
 					WechatConfig.getInstance().getAppsecret());
-			String result = HttpClientUtil.executeGet(urlStr);
+			String data = HttpClientUtil.executeGet(urlStr);
 
-			if (result != null) {
-				JSONObject jobj = JSONObject.parseObject(result);
+			if (data != null) {
+				JSONObject jobj = JSONObject.parseObject(data);
 				String token = jobj.getString("access_token");
-				return token;
+				result.put("token", token);
 			}
 		} else {
-			String urlStr = ACCESS_TOKEN_CODE_URL.replace(APPID, WechatConfig.getInstance().getAppid())
-					.replace(APPSECRET, WechatConfig.getInstance().getAppsecret()).replace("CODE", code);
-			String result = HttpClientUtil.executeGet(urlStr);
+			String urlStr = ACCESS_TOKEN_CODE_URL
+					.replace(APPID, WechatConfig.getInstance().getAppid())
+					.replace(APPSECRET,
+							WechatConfig.getInstance().getAppsecret())
+					.replace("CODE", code);
+			String data = HttpClientUtil.executeGet(urlStr);
 
-			if (result != null) {
-				JSONObject jobj = JSONObject.parseObject(result);
+			if (data != null) {
+				JSONObject jobj = JSONObject.parseObject(data);
 				String token = jobj.getString("access_token");
-				return token;
+				result.put("token", token);
+				String openid = jobj.getString("openid");
+				result.put("openid", openid);
 			}
-
 		}
-		return null;
+		return result;
 	}
 
 	public static int createMenu(String token, String menuData) {
-		String urlStr = MENU_URL.replace(ACCESS_TOKEN, getAccessToken(null));
-		String result = HttpClientUtil.executePost(urlStr, Menu.getDemoMenu());
+		String urlStr = MENU_URL.replace(ACCESS_TOKEN, token);
+		String result = HttpClientUtil.executePost(urlStr, menuData);
+		System.out.println(result);
 		return 0;
 	}
 }
